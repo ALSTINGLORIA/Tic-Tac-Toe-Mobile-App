a6f70f590235b5e8bb1af4a7ef13754487541413
alstingloria <alstingloria@gmail.com>
2024-10-12 19:01:15 +0530

    added animations

--- app/src/main/java/com/example/tic_tac_toe_app/start_screen.java
+++ app/src/main/java/com/example/tic_tac_toe_app/start_screen.java
@@ -3,8 +3,9 @@ package com.example.tic_tac_toe_app;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.View;
-import android.widget.Button;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,8 +13,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.airbnb.lottie.LottieAnimationView;
+
 public class start_screen extends AppCompatActivity {
 Intent startIntent;
+TextView gameTitle;
+LottieAnimationView splashScreenAnimation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,7 +29,12 @@ Intent startIntent;
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        gameTitle = findViewById(R.id.gameTitle);
+        splashScreenAnimation = findViewById(R.id.splashScreenAnimation);
         startIntent = new Intent(start_screen.this,ResultScreen.class);
+        Animation title_animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.title_animation);
+        gameTitle.startAnimation(title_animation);
+        splashScreenAnimation.setAnimation(R.raw.tic_tac_toe_animation);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
